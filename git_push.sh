937e729aa7085b8fc72bd3dbd38ef0665f8850e8
sbalci <drserdarbalci@gmail.com>
2023-07-01 20:23:09 +0300

    update

--- git_push.sh
+++ git_push.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
 # chmod +x git_push.sh
+# ./git_push.sh
 
-files=( $(find ./CD3_files -type f) )   # Find all files in the current directory and subdirectories
-batch_size=5000                 # Define the batch size
+# git add *.qmd
+# git commit -m "qmd files"
+# git add *.yml
+# git commit -m "yml files"
+# git push origin
+
+git lfs track "*.pdf"
+git lfs track "*.docx"
+git lfs track "*.epub"
+
+files=( $(find ./ -type f) )   # Find all files in the current directory and subdirectories
+# files=( $(find ./CD3_files -type f) )
+
+batch_size=1000                 # Define the batch size
 total_files=${#files[@]}       # Get total number of files
 batches=$((($total_files + $batch_size - 1) / $batch_size))
 
@@ -17,11 +30,16 @@ do
   fi
 
   echo "Adding files from $start to $end"
+  file_group=""
   for ((j=start; j<=end; j++))
   do
-    git add "${files[$j]}"
+    file_group+=" ${files[$j]}"
   done
 
-  git commit -m "Adding files from $start to $end"
-  git push
+  git add $file_group
+  git commit -m "WIP Adding files from $start to $end"
+  echo "Push start after adding files from $start to $end"
+  git push origin
+  echo "Pushed files from $start to $end. There are $total_files files in total."
+  sleep 20
 done
