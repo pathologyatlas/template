8b7a09087364c4c26247b183e5865fa845333306
sbalci <drserdarbalci@gmail.com>
2023-07-01 14:21:32 +0300

    Update git_push2.sh

--- git_push2.sh
+++ git_push2.sh
@@ -3,8 +3,12 @@
 # chmod +x git_push2.sh
 # ./git_push2.sh
 
+git lfs track "*.pdf"
+git lfs track "*.docx"
+git lfs track "*.epub"
+
 files=( $(find ./ -type f) )   # Find all files in the current directory and subdirectories
-batch_size=2000                 # Define the batch size
+batch_size=1000                 # Define the batch size
 total_files=${#files[@]}       # Get total number of files
 batches=$((($total_files + $batch_size - 1) / $batch_size))
 
@@ -25,9 +29,17 @@ do
   done
 
   git add $file_group
-  git commit -m "Adding files from $start to $end"
+  git commit -m "WIP Adding files from $start to $end"
   echo "Push start after adding files from $start to $end"
   git push origin
   echo "Pushed files from $start to $end"
-  sleep 10
+  sleep 20
 done
+
+
+# git add *.qmd
+# git commit -m "qmd files"
+# git add *.yml
+# git commit -m "yml files"
+# git push origin
+
