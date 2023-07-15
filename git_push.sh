b670a4fd4c42033f4fbca67c248229ebe3bd15e7
sbalci <drserdarbalci@gmail.com>
2023-07-15 21:38:35 +0300

    Update git_push.sh

--- git_push.sh
+++ git_push.sh
@@ -22,42 +22,40 @@
 # increases the git http buffer size to 500 MB
 # git config --global http.postBuffer 524288000
 
-items=( $(find ./ -type f -o -type d) )   # Find all files and directories
+files=( $(find ./ -type f) )
 
-# files=( $(find ./CD3_files -type f) )
 
 batch_size=2000
-total_items=${#items[@]}
-batches=$((($total_items + $batch_size - 1) / $batch_size))
+total_files=${#files[@]}
+batches=$((($total_files + $batch_size - 1) / $batch_size))
 
 
 start_batch=0    # Define starting batch number
-
 
 for ((i=$start_batch; i<$batches; i++))
 do
   start=$((i*batch_size))
   end=$((start+batch_size-1))
 
-  if [[ $end -gt $((total_items-1)) ]]; then
-    end=$((total_items-1))
+  if [[ $end -gt $((total_files-1)) ]]; then
+    end=$((total_files-1))
   fi
 
-  echo "Processing items from $start to $end"
+  echo "Processing files from $start to $end"
   for ((j=start; j<=end; j++))
   do
-    if [ -e "${items[$j]}" ]; then
-      git add "${items[$j]}"
+    if [ -e "${files[$j]}" ]; then
+      git add "${files[$j]}"
     else
-      git rm -r "${items[$j]}"   # The -r option allows git rm to delete directories
+      git rm "${files[$j]}"
     fi
   done
 
-  git commit -m "WIP Adding items from $start to $end"
-  echo "Committing after adding items from $start to $end. There are $total_items items in total."
+  git commit -m "WIP Adding files from $start to $end"
+  echo "Committing after adding files from $start to $end. There are $total_files files in total."
   echo "Batch number $((i+1)) is being processed."
   git push origin
-  echo "Pushed items from $start to $end. There are $total_items items in total."
+  echo "Pushed files from $start to $end. There are $total_files files in total."
   echo "Batch number $((i+1)) has been processed."
   sleep 120
 done
