47272a26ecb6d23578bb213e06762734bc08d087
sbalci <drserdarbalci@gmail.com>
2024-03-04 21:37:48 +0300

    Update generate_qmd.R

--- generate_qmd.R
+++ generate_qmd.R
@@ -595,18 +595,25 @@ yaml_string <- paste0("
 
 wsi_link_string <- '
 
+
+[https://images.patolojiatlasi.com/{{template}}/](https://images.patolojiatlasi.com/{{template}}/)
+
 [https://images.patolojiatlasi.com/{{template}}/{{stain}}.html](https://images.patolojiatlasi.com/{{template}}/{{stain}}.html)
 
 
 **{{TemplateTR}}**
 
+[Tam Ekran Görmek İçin Resmi Tıklayın](https://images.patolojiatlasi.com/{{template}}/{{stain}}.html)
 
-[![Tam Ekran Görmek İçin Resmi Tıklayın](https://www.patolojiatlasi.com/screenshots/thumbnail_{{template}}-{{stain}}.png){width="25%"}](https://images.patolojiatlasi.com/{{template}}/{{stain}}.html) [Tam Ekran Görmek İçin Resmi Tıklayın](https://images.patolojiatlasi.com/{{template}}/{{stain}}.html)
+<a href="https://images.patolojiatlasi.com/{{template}}/{{stain}}.html"><img alt="Tam Ekran Görmek İçin Resmi Tıklayın" src ="https://www.patolojiatlasi.com/screenshots/thumbnail_{{template}}-{{stain}}.png" style="width:25%;"></a>
+
 
 
 **{{TemplateEN}}**
 
-[![Click for Full Screen WSI](https://www.patolojiatlasi.com/screenshots/thumbnail_{{template}}-{{stain}}.png){width="25%"}](https://images.patolojiatlasi.com/{{template}}/{{stain}}.html) [Click for Full Screen WSI](https://images.patolojiatlasi.com/{{template}}/{{stain}}.html)
+[Click for Full Screen WSI](https://images.patolojiatlasi.com/{{template}}/{{stain}}.html)
+
+<a href="https://images.patolojiatlasi.com/{{template}}/{{stain}}.html"><img alt="Click for Full Screen WSI" src ="https://www.patolojiatlasi.com/screenshots/thumbnail_{{template}}-{{stain}}.png" style="width:25%;"></a>
 
 
 '
@@ -626,6 +633,7 @@ See Microscopy with viewer:
 
 <iframe src="https://images.patolojiatlasi.com/{{template}}/{{stain}}.html" style="height:600px;width:100%;" data-external="1"></iframe>
 
+
 '
 
 
@@ -640,6 +648,55 @@ youtube_string <-
 
 
 [Click for Video](https://www.youtube.com/watch?v={{youtube_link}})
+
+
+'
+
+
+## end_string ----
+
+end_string <-
+  '
+
+<hr>
+
+<a href="https://www.patolojiatlasi.com/"><img src ="https://www.patolojiatlasi.com/images/coverTR.png" style="width:50%;"></a>
+
+<a href="https://www.histopathologyatlas.com/"><img src ="https://www.patolojiatlasi.com/images/coverEN.png" style="width:50%;"></a>
+
+
+
+- Sosyal medyadan derlenen görüntülerden oluşan patoloji notları için [tıklayın](https://www.patolojinotlari.com/).
+
+- For social media based pathology notes [click here](https://www.patolojinotlari.com/).
+
+
+<iframe width="160" height="400" src="https://leanpub.com/patolojiatlasi/embed" frameborder="0" allowtransparency="true"></iframe>
+
+
+
+<a href="https://play.google.com/store/books/details?id=um5jEAAAQBAJ&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"><img src="https://play.google.com/intl/en_us/badges/static/images/badges/tr_badge_web_generic.png" style="width: 250px;"></a>
+
+
+
+
+<a href="https://books.apple.com/us/book/patoloji-atlas%C4%B1/id6456452218?itscg=30200&amp;itsct=books_box_badge&amp;ls=1" style="display: inline-block; overflow: hidden; border-radius: 13px; width: 250px; height: 83px;"><img src="https://tools.applemediaservices.com/api/badges/get-it-on-apple-books/badge/tr-tr?size=250x83&amp;releaseDate=1690848000" alt="Get it on Apple Books" style="border-radius: 13px; width: 250px; height: 83px;"></a>
+
+
+
+
+
+
+| Atıf için (Citation)                                                                                                                                                                                                                                                                                     |
+|------------------------------------------------------------------------|
+| [![](https://zenodo.org/badge/452585667.svg)](https://zenodo.org/badge/latestdoi/452585667)                                                                                                                                                                                                              |
+| [Open Science Framework DOI: 10.17605/OSF.IO/6W5K8](https://osf.io/6w5k8/)                                                                                                                                                                                                                               |
+| [![](https://img.shields.io/github/issues/patolojiatlasi/patolojiatlasi.github.io)](https://github.com/patolojiatlasi/patolojiatlasi.github.io/issues)                                                                                                                                                   |
+| [![](https://img.shields.io/github/license/patolojiatlasi/patolojiatlasi.github.io)](https://github.com/patolojiatlasi/patolojiatlasi.github.io/blob/main/LICENSE)                                                                                                                                       |
+| <a href="https://twitter.com/intent/tweet?text=Wow:&url=https%3A%2F%2Fwww.patolojiatlasi.com%2F%20@patolojinotlari%20@serdarbalci%20"><img src="https://img.shields.io/twitter/url?label=Patoloji%20Atlas%C4%B1&amp;style=social&amp;url=https%3A%2F%2Fwww.patolojiatlasi.com%2F" alt="Twitter"/></a>    |
+| <a href="https://twitter.com/intent/tweet?text=Wow:&url=https%3A%2F%2Fwww.patolojinotlari.com%2F%20@patolojinotlari%20@serdarbalci%20"><img src="https://img.shields.io/twitter/url?label=Patoloji%20Notlar%C4%B1&amp;style=social&amp;url=https%3A%2F%2Fwww.patolojinotlari.com%2F" alt="Twitter"/></a> |
+
+
 
 
 '
@@ -762,7 +819,7 @@ if (use_youtube) {
 
 ## end render ----
 
-readme_text <- paste(readme_text, sep = "\n\n")
+readme_text <- paste(readme_text, end_string, sep = "\n\n")
 
 
 readme_text <- gsub(pattern = "((<", replacement = "{{<", x = readme_text, fixed = TRUE)
